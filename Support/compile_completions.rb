069284a1be21bc26719e81ab40da4afd35408c3c
Tim Neems <tneems@gmail.com>
2013-02-26 23:43:06 -0500

    Remove spaces around method calls

--- Support/compile_completions.rb
+++ Support/compile_completions.rb
@@ -56,7 +56,7 @@ class RubyMotionCompletion
     @fragment.uniq!
 
     # Output results
-    plist = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><array>%s</array></plist>"  % @fragment.to_s.gsub( /\>\</, ">\n<" )
+    plist = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><array>%s</array></plist>"  % @fragment.to_s.gsub(/\>\</, ">\n<")
       
     return plist
   end
@@ -68,25 +68,25 @@ class RubyMotionCompletion
 
   # Creates a dict element
   def create_dict(display, insert=nil, match=nil)
-    elem_dict = REXML::Element.new( "dict" )
+    elem_dict = REXML::Element.new("dict")
 
     # Display
-    elem_display = REXML::Element.new( "key", elem_dict ).add_text( "display" )
-    elem_string = REXML::Element.new( "string", elem_dict ).add_text( display )
+    elem_display = REXML::Element.new("key", elem_dict).add_text("display")
+    elem_string = REXML::Element.new("string", elem_dict).add_text(display)
 
     # Insert
     if insert != nil
-      elem_display = REXML::Element.new( "key", elem_dict ).add_text( "insert" )
-      elem_string = REXML::Element.new( "string", elem_dict ).add_text( insert )
+      elem_display = REXML::Element.new("key", elem_dict).add_text("insert")
+      elem_string = REXML::Element.new("string", elem_dict).add_text(insert)
     end
     
     # Match
     if match != nil
-      elem_display = REXML::Element.new( "key", elem_dict ).add_text( "match" )
-      elem_string = REXML::Element.new( "string", elem_dict ).add_text( match )
+      elem_display = REXML::Element.new("key", elem_dict).add_text("match")
+      elem_string = REXML::Element.new("string", elem_dict).add_text(match)
     else
-      elem_display = REXML::Element.new( "key", elem_dict ).add_text( "match" )
-      elem_string = REXML::Element.new( "string", elem_dict ).add_text( display.chomp( ":" ) )
+      elem_display = REXML::Element.new("key", elem_dict).add_text("match")
+      elem_string = REXML::Element.new("string", elem_dict).add_text(display.chomp(":"))
     end
 
     return elem_dict
@@ -101,20 +101,20 @@ class RubyMotionCompletion
     arguments = []
 
     # Create the prefixes array
-    if method.get_elements( "arg" ).length > 1
-      prefixes = method_name.split( ":" )
+    if method.get_elements("arg").length > 1
+      prefixes = method_name.split(":")
       prefixes[0] = nil
     end
     
     # Create the arguments array
-    method.each_element( "arg" ) do |param|
+    method.each_element("arg") do |param|
       prefix = ""
 
       # Construct prefix
       prefix = prefixes[i] + ":" if prefixes[i] != nil
 
       # Add argument to the array
-      arguments << "%s${%d:%s %s}" % [ prefix, idx, param.attribute( "declared_type" ).to_s, param.attribute( "name" ).to_s ]
+      arguments << "%s${%d:%s %s}" % [ prefix, idx, param.attribute("declared_type").to_s, param.attribute("name").to_s ]
 
       # Increase counters
       i += 1
@@ -122,7 +122,7 @@ class RubyMotionCompletion
     end
     
     # Construct insert string
-    insert = arguments.join( ", " )
+    insert = arguments.join(", ")
     insert = "(%s)" % insert unless insert == ""
     
     return insert
@@ -130,32 +130,32 @@ class RubyMotionCompletion
 
   # Returns a valid class definition
   def parse_class(node)
-    class_name = node.attribute( "name" ).to_s
+    class_name = node.attribute("name").to_s
 
     # Add the main class to the @fragment
-    @fragment << create_dict( class_name )
+    @fragment << create_dict(class_name)
 
     # Traverse class methods
-    node.each_element( "method" ) do |method|
+    node.each_element("method") do |method|
       
       # Prepend method name with class name if this is a class method
-      method_name = method.attribute( "selector" ).to_s
-      method_name = "%s.%s" % [ class_name, method_name ] if method.attribute( "class_method" )
+      method_name = method.attribute("selector").to_s
+      method_name = "%s.%s" % [ class_name, method_name ] if method.attribute("class_method")
 
       # Check for the number of arguments
-      case method.get_elements( "arg" ).length
+      case method.get_elements("arg").length
         
         # No arguments so strip the ':' if there is one
         when 0
-          @fragment << create_dict( method_name )
+          @fragment << create_dict(method_name)
         
         # A single argument
         when 1
-          @fragment << create_dict( method_name, self.create_insert( method_name, method ) )
+          @fragment << create_dict(method_name, self.create_insert(method_name, method))
 
         else
-          method_match = method_name.slice( 0, method_name.index( ":" ) )
-          @fragment << create_dict( method_name, self.create_insert( method_name, method ), method_match )
+          method_match = method_name.slice(0, method_name.index(":"))
+          @fragment << create_dict(method_name, self.create_insert(method_name, method), method_match)
 
       end
 
@@ -164,26 +164,26 @@ class RubyMotionCompletion
 
   # Returns a valid class definition
   def parse_function(node)
-    function_name = node.attribute( "name" ).to_s
+    function_name = node.attribute("name").to_s
 
     # Check for the number of arguments
-    case node.get_elements( "arg" ).length
+    case node.get_elements("arg").length
       
       # No arguments so strip the ':' if there is one
       when 0
-        @fragment << create_dict( function_name )
+        @fragment << create_dict(function_name)
       
       # More than one argument
       else
-        @fragment << create_dict( function_name, self.create_insert( function_name, node ) )
+        @fragment << create_dict(function_name, self.create_insert(function_name, node))
 
     end
   end
 
   # Returns a valid constant definition
   def parse_constant(node)
-    const_match = node.attribute( "name" ).to_s
-    const_type = node.attribute( "declared_type" ).to_s
+    const_match = node.attribute("name").to_s
+    const_type = node.attribute("declared_type").to_s
 
     # Make sure the first letter is always uppercase, for RubyMotion
     const_match = "%s%s" % [ const_match[0,1].upcase, const_match[1..-1] ]
@@ -191,13 +191,13 @@ class RubyMotionCompletion
     const_display = "%s (%s)" % [ const_match, const_type ]
 
     # Add the element
-    @fragment << create_dict( const_display, nil, const_match )
+    @fragment << create_dict(const_display, nil, const_match)
   end
 
   # Returns a valid enum definition
   def parse_enum(node)
-    enum_match = node.attribute( "name" ).to_s
-    enum_val = node.attribute( "value" ).to_s
+    enum_match = node.attribute("name").to_s
+    enum_val = node.attribute("value").to_s
 
     # Make sure the first letter is always uppercase, for RubyMotion
     enum_match = "%s%s" % [ enum_match[0,1].upcase, enum_match[1..-1] ]
@@ -205,7 +205,7 @@ class RubyMotionCompletion
     enum_display = "%s (%s)" % [ enum_match, enum_val ]
 
     # Add the element
-    @fragment << create_dict( enum_display, nil, enum_match )
+    @fragment << create_dict(enum_display, nil, enum_match)
   end
 
   def ruby_motion_root
